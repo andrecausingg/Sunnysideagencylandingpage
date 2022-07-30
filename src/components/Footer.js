ad6dd71818a8392306b4d4de7be3dfe23db41b8c
andrecausingg <95618374+andrecausingg@users.noreply.github.com>
2022-07-30 20:50:24 +0800

    change footer style

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,14 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {faFacebookSquare, faInstagram, faTwitter, faPinterest} from '@fortawesome/free-brands-svg-icons'
 
+// Css
+import '../scss/style.css';
+
 // Image
 import logo from '../images/logo.svg' // Tell webpack this JS file uses this image
 
 const Footer = () => {
+
   return (
     <footer className='yot-bg-moderate-cyan '>
         <div className='yot-container yot-flex yot-flex-fd-c-ai-c-jc-c yot-content-space-padding-tb-80'>
@@ -12,29 +16,29 @@ const Footer = () => {
                 <img src={logo} alt='Logo' style={{width: '173px', height:'38px'}}/>
             </div>
             <div>
-                <span>About</span>
-                <span className='yot-margin-lr-48'>Services</span>
-                <span>Project</span>
+                <span className='hover-links'>About</span>
+                <span className='yot-margin-lr-48 hover-links'>Services</span>
+                <span className='hover-links'>Project</span>
             </div>
-            <div className='yot-margin-t-48 '>
+            <div className='yot-margin-t-48'>
               <FontAwesomeIcon 
-                  icon={faFacebookSquare} 
-                  style={{fontSize:'36px', color:'hsl(167, 40%, 24%)'}}
+                icon={faFacebookSquare} 
+                className='hoverIcon'
               />
 
               <FontAwesomeIcon 
-                  icon={faInstagram} className='yot-margin-lr-24' 
-                  style={{fontSize:'36px', color:'hsl(167, 40%, 24%)'}}
+                  icon={faInstagram} 
+                  className='yot-margin-lr-24 hoverIcon' 
               />
 
               <FontAwesomeIcon 
-                  icon={faTwitter} className='yot-margin-r-24' 
-                  style={{fontSize:'36px', color:'hsl(167, 40%, 24%)'}}
+                  icon={faTwitter} 
+                  className='yot-margin-r-24 hoverIcon' 
               />
 
               <FontAwesomeIcon 
                   icon={faPinterest} 
-                  style={{fontSize:'36px', color:'hsl(167, 40%, 24%)'}}
+                  className='hoverIcon'
               />
             </div>
         </div>
